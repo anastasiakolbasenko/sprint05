cc24848db19aacfffccd024ba2f4bc90248ee541
Anastasia Kolbasenko <anastasiakolbasenko@Anastasias-MacBook-Pro.local>
2021-06-14 21:42:50 +0300

    commit

--- t04_anonymous/index.js
+++ t04_anonymous/index.js
@@ -1,3 +1,30 @@
+const Anonymous = class {
+    constructor() {}
+
+    getAnonymous(name, alias, affiliation) {
+        return {name, alias, affiliation};
+    }
+
+};
+
+module.exports = new Anonymous();
+
+
+const i = require('./index');
+
+const mandarin = i.getAnonymous('Unknown', 'Mandarin', 'Ten Rings');
+
+console.log([
+    mandarin.name,
+    mandarin.alias,
+    mandarin.affiliation,
+].join('\n'));
+
+/* test */
+/*
+console.log( '\n' + (mandarin.affiliation).toUpperCase() );
+console.log( (mandarin.alias).toUpperCase() );
+console.log( (mandarin.name).toUpperCase() );*/
 
 
 
@@ -16,14 +43,3 @@
 
 
 
-
-
-//const i = require('./index');
-
-// const mandarin = i.getAnonymous('Unknown', 'Mandarin', 'Ten Rings');
-
-// console.log([
-//     mandarin.name,
-//     mandarin.alias,
-//     mandarin.affiliation,
-// ].join('\n'));
